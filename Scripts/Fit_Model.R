02bcc97ffb176aa9607ec96358f7c5b31a05d2ca
jinhangjiang <60441304+jinhangjiang@users.noreply.github.com>
2020-05-13 17:03:13 -0500

    Update Fit_Model.R

--- Scripts/Fit_Model.R
+++ Scripts/Fit_Model.R
@@ -11,13 +11,13 @@ a[,c("TITLE","RELEASE_DATE_TXT","PRODUCTION_COMPANIES",
 write.csv(a, file = "modeldata.csv")
 
 
-
 ### average rating prediction
 a1<-a
 
 a1[,c("BUDGET","REVENUE","VOTE_COUNT","VOTE_AVERAGE",
-      "ML_RCOUNT","ML_RATING","PERFORMANCE", "RELEVANCE_MEAN")]<-list(NULL)
+      "ML_RCOUNT","ML_RATING","PERFORMANCE", "RELEVANCE_Mean","X")]<-list(NULL)
 a1<-na.omit(a1)
+
 set.seed(1)
 index<-sample(nrow(a1), 0.8*nrow(a1))
 a1.train<-a1[index,-1]
@@ -29,15 +29,105 @@ null<-lm(a1.train$AVERAGERATING~1, data = a1.train)
 full<-lm(a1.train$AVERAGERATING~., data = a1.train)
 step(null, list(lower = null, upper = full), direction = c("both"))
 
-fit<-lm(formula = a1.train$AVERAGERATING ~ ML_RCOUNTlog + STARTYEAR + 
-     documentary + drama + horror + RELEVANCE_Mean_1 + sci.fi + 
-     action + RUNTIME + NO_GENRE + NUMVOTESlog + Actor1_Sex + 
-     budget_log + animation + REVENUElog + western + 
-     adventure + film.noir + children + mystery + fantasy, data = a1.train)
+fit<-lm(formula = a1.train$AVERAGERATING ~ STARTYEAR + 
+          documentary + drama + horror + RELEVANCE_Mean_1 + sci.fi + 
+          action + RUNTIME + NO_GENRE + NUMVOTESlog + Actor1_Sex + 
+          budget_log + animation + NUMVOTES + REVENUElog + western + 
+          adventure + film.noir + children + mystery + fantasy + romance, 
+        data = a1.train)
 summary(fit)
+
+library(MASS)
+a1.train$stres=stdres(fit)
+subset(a1.train, a1.train$stres>3)
+subset(a1.train, a1.train$stres< -3)
+a1.train.subset<-subset(a1.train, a1.train$stres<3 & a1.train$stres> -3)
+
+
+fit<-lm(formula = AVERAGERATING ~ STARTYEAR + 
+          documentary + drama + horror + RELEVANCE_Mean_1 + sci.fi + 
+          action + RUNTIME + NO_GENRE + NUMVOTESlog + Actor1_Sex + 
+          budget_log + animation + NUMVOTES + REVENUElog + western + 
+          adventure + film.noir, 
+        data = a1.train.subset)
+summary(fit)
+
+
+a1.train.subset$stres=stdres(fit)
+subset(a1.train.subset, a1.train.subset$stres>3)
+subset(a1.train.subset, a1.train.subset$stres< -3)
+a1.train.subset2<-subset(a1.train.subset, a1.train.subset$stres<3 & a1.train.subset$stres> -3)
+
+
+fit<-lm(formula = AVERAGERATING ~ STARTYEAR + 
+          documentary + drama + horror + RELEVANCE_Mean_1 + sci.fi + 
+          action + RUNTIME + NO_GENRE + NUMVOTESlog + Actor1_Sex + 
+          budget_log + animation + NUMVOTES + REVENUElog + western + 
+          adventure + film.noir, 
+        data = a1.train.subset2)
+summary(fit)
+
+a1.train.subset2$stres=stdres(fit)
+subset(a1.train.subset2, a1.train.subset2$stres>3)
+subset(a1.train.subset2, a1.train.subset2$stres< -3)
+a1.train.subset3<-subset(a1.train.subset2, a1.train.subset2$stres<3 & a1.train.subset2$stres> -3)
+
+
+fit<-lm(formula = AVERAGERATING ~ STARTYEAR + 
+          documentary + drama + horror + RELEVANCE_Mean_1 + sci.fi + 
+          action + RUNTIME + NO_GENRE + NUMVOTESlog + Actor1_Sex + 
+          budget_log + animation + NUMVOTES + REVENUElog + western + 
+          adventure + film.noir, 
+        data = a1.train.subset3)
+summary(fit)
+
+
+a1.train.subset3$stres=stdres(fit)
+subset(a1.train.subset3, a1.train.subset3$stres>3)
+subset(a1.train.subset3, a1.train.subset3$stres< -3)
+a1.train.subset4<-subset(a1.train.subset3, a1.train.subset3$stres<3 & a1.train.subset3$stres> -3)
+
+fit<-lm(formula = AVERAGERATING ~ STARTYEAR + 
+          documentary + drama + horror + RELEVANCE_Mean_1 + sci.fi + 
+          action + RUNTIME + NO_GENRE + NUMVOTESlog + Actor1_Sex + 
+          budget_log + animation + NUMVOTES + REVENUElog + western + 
+          adventure, 
+        data = a1.train.subset4)
+summary(fit)
+
+a1.train.subset4$stres=stdres(fit)
+subset(a1.train.subset4, a1.train.subset4$stres>3)
+subset(a1.train.subset4, a1.train.subset4$stres< -3)
+a1.train.subset5<-subset(a1.train.subset4, a1.train.subset4$stres<3 & a1.train.subset4$stres> -3)
+
+
+fit<-lm(formula = AVERAGERATING ~ STARTYEAR + 
+          documentary + drama + horror + RELEVANCE_Mean_1 + sci.fi + 
+          action + RUNTIME + NO_GENRE + NUMVOTESlog + Actor1_Sex + 
+          budget_log + animation + NUMVOTES + REVENUElog + western + 
+          adventure, 
+        data = a1.train.subset5)
+summary(fit)
+
+
+a1.train.subset5$stres=stdres(fit)
+subset(a1.train.subset5, a1.train.subset5$stres>3)
+subset(a1.train.subset5, a1.train.subset5$stres< -3)
+a1.train.subset6<-subset(a1.train.subset5, a1.train.subset5$stres<3 & a1.train.subset5$stres> -3)
+
+fit<-lm(formula = AVERAGERATING ~ STARTYEAR + 
+          documentary + drama + horror + RELEVANCE_Mean_1 + sci.fi + 
+          action + RUNTIME + NO_GENRE + NUMVOTESlog + Actor1_Sex + 
+          budget_log + animation + NUMVOTES + REVENUElog + western + 
+          adventure, 
+        data = a1.train.subset6)
+summary(fit)
+
 
 plot(fit$res~fit$fitted)
 hist(fit$res)
+qqnorm(fit$res)
+qqline(fit$res)
 AIC(fit)
 
 pred1<-predict(fit, newdata = a1.test)
@@ -141,3 +231,55 @@ fit3<-glm(formula = a2$PERFORMANCE ~ RELEVANCE_Mean_1 + documentary +
 
 cv1<-cv.glm(data = a2, glmfit = fit3, cost = auc, K=10)
 cv1$delta[2]
+
+
+### find ROI
+movie<-read.csv("modeldata.csv")
+movie[,c("X","RELEVANCE_Mean")]<-NULL
+
+a<-movie[movie$STARTYEAR>=1990,]
+
+a<-a[a$REVENUE!=0,]
+a<-a[a$BUDGET!=0,]
+a$ROI<-((a$REVENUE)-(a$BUDGET))/a$BUDGET
+cor(a$ROI, a$REVENUE, method = "pearson")
+
+
+set.seed(2000)
+index<-sample(nrow(a), 0.8*nrow(a))
+a3<-na.omit(a)
+
+
+a3.train <-a3[index,-1]
+a3.test <-a3[-index,-1]
+
+
+null<-lm(ROI~1, data = a3.train.subset)
+full<-lm(ROI~., data = a3.train.subset)
+step(null, list(lower = null, upper = full), direction = c("both"))
+
+fit4<-lm(formula = a3.train$ROI ~ documentary + 
+           log(BUDGET) + Actor1_Sex + log(REVENUE), data = a3.train)
+summary(fit4)
+
+crash$stres=stdres(fit1)
+subset(crash,crash$stres< -3)
+subset(crash,crash$stres > 3)
+
+library(MASS)
+a3.train$stres=stdres(fit4)
+subset(a3.train, a3.train$stres>3)
+subset(a3.train, a3.train$stres< -3)
+a3.train.subset<-subset(a3.train, a3.train$stres<3 & a3.train$stres> -3)
+
+fit4<-lm(formula = ROI ~ documentary  + NUMVOTESlog + 
+           BUDGET + REVENUE + musical + VOTE_COUNT , data = a3.train.subset)
+summary(fit4)
+
+plot(fit4$res~fit4$fitted)
+hist(fit4$res)
+AIC(fit4)
+
+pred4<-predict(fit4, newdata = a3.test)
+MSPE4<-mean((a3.test$ROI-pred4)^2)
+MSPE4
