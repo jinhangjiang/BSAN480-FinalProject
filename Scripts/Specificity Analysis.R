0d41cf4db1ee4d5f7a10fb62c1640a199d483061
jinhangjiang <60441304+jinhangjiang@users.noreply.github.com>
2020-05-13 19:00:47 -0500

    Update Specificity Analysis.R

--- Scripts/Specificity Analysis.R
+++ Scripts/Specificity Analysis.R
@@ -11,3 +11,14 @@ spacy_initialize(model = 'en_core_web_sm')
 document<-spacy_parse(as.character(text))
 document_entity<-entity_extract(document,type = "all")
 nrow(document_entity)/sum(str_count(unlist(text), '\\w+'))
+
+
+tag.bottom<-tag[tag$RELEVANCE<=0.012,]
+document1<-spacy_parse(as.character(tag.bottom$TAG))
+document_entity1<-entity_extract(document1,type = "all")
+nrow(document_entity1)/sum(str_count(unlist(tag.bottom$TAG), '\\w+'))
+
+a<-c(0.04143767, 0.2300687)
+b<-c(mean(tag.top$RELEVANCE), mean(tag.bottom$RELEVANCE))
+cor(a,b,method = "pearson")
+plot(a~b, type="l")
